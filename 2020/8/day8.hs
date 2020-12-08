c5f58b04c72734339147dc0234e3d0f272509055
Igor Bondarenko <i@jetmind.me>
2020-12-08 09:16:52 +0200

    2020/8/2

--- 2020/8/day8.hs
+++ 2020/8/day8.hs
@@ -1,8 +1,9 @@
+import Data.Either
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 
 
-data Instruction = ACC Int | JMP Int | NOP deriving Show
+data Instruction = ACC Int | JMP Int | NOP Int deriving Show
 type Program     = Map.Map Int Instruction
 type Memory      = Set.Set Int
 
@@ -10,7 +11,7 @@ type Memory      = Set.Set Int
 makeOp :: String -> Int -> Instruction
 makeOp "acc" n = ACC n
 makeOp "jmp" n = JMP n
-makeOp "nop" _ = NOP
+makeOp "nop" n = NOP n
 
 
 compileInstruction :: String -> Instruction
@@ -28,21 +29,36 @@ compile input = Map.fromList withIndex
         instructions = map compileInstruction $ lines input
 
 
-runLoop :: Program -> Memory -> Int -> Int -> Int
+runLoop :: Program -> Memory -> Int -> Int -> Either Int Int
 runLoop p mem acc ip 
-  | Set.member ip mem = acc
+  | Set.member ip mem = Left acc  -- found a loop
   | otherwise         = case Map.lookup ip p of
-                          Nothing      -> acc
+                          Nothing      -> Right acc  -- reached the end
                           Just (ACC n) -> runLoop p newMem (acc + n) (ip + 1)
                           Just (JMP n) -> runLoop p newMem acc (ip + n)
-                          Just NOP     -> runLoop p newMem acc (ip + 1)
+                          Just (NOP _) -> runLoop p newMem acc (ip + 1)
     where newMem = Set.insert ip mem
 
 
-run :: Program -> Int
+correct :: Program -> [Program] -> Int -> [Program]
+correct p ps ip
+  | ip >= Map.size p = ps
+  | otherwise        = correct p newPs (ip + 1)
+    where newPs = case Map.lookup ip p of
+                    Just (JMP n) -> (Map.insert ip (NOP n) p) : ps
+                    Just (NOP n) -> (Map.insert ip (JMP n) p) : ps
+                    _            -> ps
+
+
+run :: Program -> Either Int Int
 run p = runLoop p Set.empty 0 0
+
+runCorrected :: Program -> Int
+runCorrected p = head $ rights $ map run programs
+  where programs = correct p [] 0
 
 
 main = do
   f <- readFile "input.txt"
   print $ run $ compile f
+  print $ runCorrected $ compile f
