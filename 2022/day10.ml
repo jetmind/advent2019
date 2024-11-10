b73ec3bde02166d36c565e938c05a5aa021452f8
Igor Bondarenko <i@jetmind.me>
2024-11-11 00:24:27 +0200

    2022/10/2

--- 2022/day10.ml
+++ 2022/day10.ml
@@ -1,27 +1,34 @@
 open Base
 open Stdio
 
-type cpu_t = { x: int;
-               cycles: int;
-             } [@@deriving show]
+let x        = ref 1
+let cycle    = ref 1
+let strength = ref 0
+let screen   = ref ""
 
-let initcpu : cpu_t = { x = 1; cycles = 1 }
+let tick () =
+  let pixel = (!cycle - 1) % 40 in
+  printf "cycle=%i, pixel=%i\n" !cycle pixel;
+  if pixel = !x || pixel = !x - 1 || pixel = !x + 1
+  then screen := !screen ^ "#"
+  else screen := !screen ^ ".";
+  if !cycle % 40 = 0 then screen := !screen ^ "\n";
+  cycle := !cycle + 1
 
-let eval ({x; cycles}, history) line =
+let eval line =
   match String.split line ~on:' ' with
   | ["addx"; arg] ->
     let n = Int.of_string arg in
-    let current = { x = x + n; cycles = cycles + 2 } in
-    let history' = current :: { x = x; cycles = cycles + 1 } :: history in
-    (current, history')
-  | ["noop"] ->
-    let current = { x = x; cycles = cycles + 1 } in
-    (current, current :: history)
-  | _ -> failwith ("invalid input: " ^ line)
+    for i = 1 to 2 do
+      tick ();
+      if i = 2 then x := !x + n;
+      match !cycle with
+      | 20 | 60 | 100 | 140 | 180 | 220 -> strength := !strength + !x * !cycle
+      | _                               -> ()
+    done;
+  | ["noop"] -> tick ();
+  | _        -> failwith ("invalid input: " ^ line)
 
-(* let _ = "input10ex1.txt" |> In_channel.read_all |> String.split_lines |> List.fold ~init:(initcpu, [initcpu]) ~f:eval |> [%show: cpu_t * cpu_t list] |> printf "%s\n" *)
-let () = "input10.txt" |> In_channel.read_all |> String.split_lines
-         |> List.fold ~init:(initcpu, [initcpu]) ~f:eval
-         |> snd
-         |> List.sum (module Int) ~f:(fun {x; cycles} -> match cycles with 20 | 60 | 100 | 140 | 180 | 220 -> x * cycles | _ -> 0)
-         |> printf "%i\n"
+let () = "input10.txt" |> In_channel.read_all |> String.split_lines |> List.iter ~f:eval
+let () = printf "%i\n" !strength
+let () = printf "%s\n" !screen
